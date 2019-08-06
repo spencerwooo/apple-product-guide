d54ad4bb6850cbfe402c0380f9bf320b9c76aac3
spencerwooo <spencerwoo98@gmail.com>
2019-08-06 22:49:51 +0800

    feat: update data every hour

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/robfig/cron"
 )
 
 const iammain string = "[Main]"
 const port string = "8000"
 
 func main() {
+	initUpdateTask()
+
+	fmt.Printf("%s Starting server on %s...\n", iammain, "localhost:"+port)
+	serve()
+}
+
+func initUpdateTask() {
+	// Create cron task to update data hourly
+	c := cron.New()
+	c.AddFunc("@hourly", updateData)
+	c.Start()
+}
+
+func updateData() {
 	fmt.Printf("%s Starting crawler...\n", iammain)
 
 	url := "https://buyersguide.macrumors.com"
@@ -18,7 +34,4 @@ func main() {
 	parseResponse(doc)
 
 	fmt.Printf("%s Latest data updated.\n", iammain)
-
-	fmt.Printf("%s Starting server on %s...\n", iammain, "localhost:"+port)
-	serve()
 }
